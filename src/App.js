d22538ca4a7ffece31ec358783bd5f4aa87ce1ee
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-16 15:21:10 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [corn, setCorn] = useState(0)
+  function farmCorn(){
+    setCorn(corn + 1);
+  }
+  function sellCorn(){
+    setCorn(corn - 1);
+  }
+  function abolishCorn(){
+    setCorn(corn - corn);
+  }
+ return(
+   <div className="App">
+     <input type="number" id="name" name="name"/>
+     <button type="button" onClick={farmCorn}>+1 Corn</button>
+     <button type="button" onClick={abolishCorn}>Abolish Corn</button>
+     <button type="button" onClick={sellCorn}>-1 Corn</button>
+     <h1>{corn}</h1>
+   </div>
+ )
 }
 
 export default App;
